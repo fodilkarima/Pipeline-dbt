efe411e58409d104c8e1b33592357ba5120e5708
fodilkarima <fodil.karima@hotmail.com>
2026-03-13 23:23:22 +0100

    Create unique_user_year.sql

---
+++ tests/unique_user_year.sql
@@ -0,0 +1,12 @@
+-- Test de qualité des données
+-- Vérifie que chaque étudiant apparaît au maximum une fois par année
+-- La combinaison (user_id, year_path_started) doit être unique
+select
+    user_id,
+    year_path_started,
+    count(*) as nb
+from {{ ref('stg_etudiant') }}
+group by 1, 2
+-- Si le nombre est supérieur à 1, cela signifie qu'il existe des doublons
+-- Dans ce cas le test échoue
+having count(*) > 1
